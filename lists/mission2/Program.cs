66bc38dffcff30e6a1a2eb5d092cd73a55967ab3
Lucia/Lukas Ekberg <lukkan99@gmail.com>
2025-03-20 15:02:52 +0100

    list mission 2 part 2

--- lists/mission2/Program.cs
+++ lists/mission2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 var random = new Random();
 int basiliskHp = 16;
+int warriorTurn = 0;
 List<string> warriors = ["Garry", "Rin", "Hougen", "Selander"];//yes the names based on my cats
 for(int a = 0; a < 8; a++ )
 {
@@ -8,9 +9,21 @@ for(int a = 0; a < 8; a++ )
 }
 
 Console.WriteLine($"Fighters {warriors[0]} {warriors[1]} {warriors[2]} {warriors[3]} enters the dungon.\nA basilisk with {basiliskHp}HP appears!");
-foreach(string warrior in warriors)
+while(basiliskHp > 0)
 {
+    if(warriorTurn == 4)
+    {
+        warriorTurn = 0;
+    }
     int atack = random.Next(1,7) + random.Next(1,7);
     basiliskHp = basiliskHp - atack;
-    Console.WriteLine($"{warrior} hits the baslisk for {atack}. Baslisk has {basiliskHp}HP left!");
+    if(basiliskHp < 0)
+    {
+        Console.WriteLine($"{warriors[warriorTurn]} hits the baslisk for {atack}. Baslisk has 0 HP left!\nThe basslisk Falls and dies The warriors has a feast!");
+    }
+    else
+    {
+        Console.WriteLine($"{warriors[warriorTurn]} hits the baslisk for {atack}. Baslisk has {basiliskHp}HP left!");
+        warriorTurn = warriorTurn + 1;
+    }
 }
