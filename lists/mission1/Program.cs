f725efc9dd42d9f286fabd575d3e0d7655537265
Lucia/Lukas Ekberg <lukkan99@gmail.com>
2025-03-20 11:14:46 +0100

    finnished mission 1 of lists

--- lists/mission1/Program.cs
+++ lists/mission1/Program.cs
@@ -3,4 +3,13 @@ var random = new Random();
 
 List<int> dices = [random.Next(1,7), random.Next(1,7), random.Next(1,7), random.Next(1,7)];
 List<int> ablityScore = [];
-
+for(int a=0; a < 6; a++)
+{
+    Console.Write($"You rolled {dices[0]} {dices[1]} {dices[2]} {dices[3]} ");
+    dices.Sort();
+    ablityScore.Add(dices[1] + dices[2] + dices[3]);
+    Console.WriteLine($"The ability score is {dices[1] + dices[2] + dices[3]}");
+    dices = [random.Next(1,7), random.Next(1,7), random.Next(1,7), random.Next(1,7)];
+}
+ablityScore.Sort();
+Console.WriteLine($"Your available ability scores are {ablityScore[0]} {ablityScore[1]} {ablityScore[2]} {ablityScore[3]} {ablityScore[4]} {ablityScore[5]}");
