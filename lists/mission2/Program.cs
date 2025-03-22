6dc8052a6a3ccf5fa7dd52eaa23c044d081b34ab
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-03-22 16:11:14 +0100

    mission 2 final

--- lists/mission2/Program.cs
+++ lists/mission2/Program.cs
@@ -2,18 +2,35 @@
 var random = new Random();
 int basiliskHp = 16;
 int warriorTurn = 0;
+int pickHero;
 List<string> warriors = ["Garry", "Rin", "Hougen", "Selander"];//yes the names based on my cats
+Console.WriteLine(warriors.Count);
 for(int a = 0; a < 8; a++ )
 {
     basiliskHp = basiliskHp + random.Next(1,9);
 }
 
 Console.WriteLine($"Fighters {warriors[0]} {warriors[1]} {warriors[2]} {warriors[3]} enters the dungon.\nA basilisk with {basiliskHp}HP appears!");
-while(basiliskHp > 0)
+while(basiliskHp > 0 && warriors.Count > 0)
 {
-    if(warriorTurn == 4)
+    if(warriorTurn == warriors.Count)
     {
-        warriorTurn = 0;
+        pickHero = random.Next(0,warriors.Count);
+        Console.WriteLine($"The basilisk uses petrifying gaze on {warriors[pickHero]}");
+        int conthrow = random.Next(1,21);
+        if(conthrow + 3 < 12)
+        {
+            Console.WriteLine($"{warriors[pickHero]} rolled {conthrow} and failed be saved. {warriors[pickHero]} got turn into stone.");
+            warriors.RemoveAt(pickHero);
+        }
+        else
+        {
+            Console.WriteLine($"{warriors[pickHero]} rolled {conthrow} and is saved from the atrack.");
+        }
+        if(warriors.Count > 0)
+        {
+            warriorTurn = 0;
+        }
     }
     int atack = random.Next(1,5);
     basiliskHp = basiliskHp - atack;
@@ -23,7 +40,14 @@ while(basiliskHp > 0)
     }
     else
     {
-        Console.WriteLine($"{warriors[warriorTurn]} hits the baslisk for {atack}. Baslisk has {basiliskHp}HP left!");
-        warriorTurn = warriorTurn + 1;
+        if(warriors.Count > 0)
+        {
+            Console.WriteLine($"{warriors[warriorTurn]} hits the baslisk for {atack}. Baslisk has {basiliskHp}HP left!");
+            warriorTurn = warriorTurn + 1;
+        }
+        else
+        {
+            Console.WriteLine($"The party has failed and the basilisk continues to turn unsuspecting adventurers to stone.");
+        }
     }
 }
