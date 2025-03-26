91b515ad876467cddccb5aa9b125997e7fbf1f40
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-03-26 14:46:03 +0100

    enums mission 1

---
+++ Enums/Mission1/Program.cs
@@ -0,0 +1,38 @@
+﻿enum Suit 
+{
+    Heart,
+    Spade,
+    Diamond,
+    Club
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        static void DrawAce(Suit suit)
+        {
+            
+            if(suit == Suit.Heart)
+            {
+                Console.WriteLine("╭───────╮\n│A      │\n│ ♥     │\n│    ♥  │\n│      ♥│\n│      A│\n╰───────╯");
+            }
+            if(suit == Suit.Spade)
+            {
+                Console.WriteLine("╭───────╮\n│A      │\n│ ♠     │\n│    ♠  │\n│      ♠│\n│      A│\n╰───────╯");
+            }
+            if(suit == Suit.Diamond)
+            {
+                Console.WriteLine("╭───────╮\n│A      │\n│ ♦     │\n│    ♦  │\n│      ♦│\n│      A│\n╰───────╯");
+            }
+            if(suit == Suit.Club)
+            {
+                Console.WriteLine("╭───────╮\n│A      │\n│ ♣     │\n│    ♣  │\n│      ♣│\n│      A│\n╰───────╯");
+            }
+        }
+        DrawAce(Suit.Heart);
+        DrawAce(Suit.Diamond);
+        DrawAce(Suit.Club);
+        DrawAce(Suit.Spade);
+        
+    }
+}
