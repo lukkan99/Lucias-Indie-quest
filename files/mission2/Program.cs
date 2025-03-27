85655cfa3a9cb213ab79ae42ddda5bd4c359ddcd
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-03-27 16:43:56 +0100

    files mission 2

---
+++ files/mission2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+string[]backers = File.ReadAllLines("backers.txt");
+string playername = "";
+if(File.Exists("player.txt"))
+{
+    playername = File.ReadAllText("player.txt");
+    Console.WriteLine($"Welcome back, {playername}, let's continue!");
+}
+else
+{
+    Console.WriteLine("Welcome to your biggest adventure yet!\n\n What your name?");
+    playername = Console.ReadLine();
+
+    File.WriteAllText("player.txt", playername);
+}
+if(Array.IndexOf(backers,playername) == -1)
+{
+    Console.WriteLine($"Unfortunately I cannot let you into Dr. Fred's secret laboratory.");
+}
+else
+{
+    Console.WriteLine($"You successfully enter Dr. Fred's secret laboratory and are greeted with a warm welcome for backing the game's Kickstarter!");
+}
