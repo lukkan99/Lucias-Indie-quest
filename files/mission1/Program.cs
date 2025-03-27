06ed19e77cd1b633db58c490be4a11e77a2a38ef
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-03-27 13:33:24 +0100

    files mission 1

---
+++ files/mission1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+string playername = "";
+if(File.Exists("player.txt"))
+{
+    playername = File.ReadAllText("player.txt");
+    Console.WriteLine($"Welcome back, {playername}, let's continue!");
+}
+else
+{
+    Console.WriteLine("Welcome to your biggest adventure yet!\n\n What your name?");
+    playername = Console.ReadLine();
+
+    File.WriteAllText("player.txt", playername);
+}
