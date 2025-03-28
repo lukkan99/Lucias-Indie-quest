9c425ed7d3a6407f3e39487c8f86530a993ad7b1
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-03-28 15:38:50 +0100

    switch

---
+++ cases/mission1/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Write("Set price: ");
+string userOperations = "";
+int totalprice;
+string userinput = Console.ReadLine();
+List<string> userinputarray = userinput.Split().ToList();
+if((userinputarray.Count() > 3) ||((userinputarray.Count() > 1)&&(userinputarray.Count() < 3)))
+{
+    Console.WriteLine("invallid format");
+}
+else
+{
+    if(userinputarray.Count() == 3)
+    {
+        userOperations = userinputarray[1];
+    }
+    switch(userOperations)
+    {
+        case "+":
+        case "plus":
+        totalprice = int.Parse(userinputarray[0]) + int.Parse(userinputarray[2]);
+        Console.WriteLine($"Total price : {totalprice}");
+        break;
+
+        case "-":
+        case "minus":
+        totalprice = int.Parse(userinputarray[0]) - int.Parse(userinputarray[2]);
+        Console.WriteLine($"Total price : {totalprice}");
+        break;
+
+        case "/":
+        case "divide":
+        totalprice = int.Parse(userinputarray[0]) / int.Parse(userinputarray[2]);
+        Console.WriteLine($"Total price : {totalprice}");
+        break;
+
+        case "*":
+        case "multiply":
+        totalprice = int.Parse(userinputarray[0]) * int.Parse(userinputarray[2]);
+        Console.WriteLine($"Total price : {totalprice}");
+        break;
+
+        default:
+        Console.WriteLine($"Total price : {userinputarray[0]}");
+        break;
+        
+
+    }
+}
