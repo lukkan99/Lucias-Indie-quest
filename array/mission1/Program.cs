55eeda43ba8d66f68fe8c6dcbc7614db5d0ae19d
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-04-01 16:42:30 +0200

    array mission 1

---
+++ array/mission1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+static string CreateDayDescription(int day, int season, int year)
+{
+string[] Seasons = ["Spring","Summer","Fall","Winter"];
+string theDay;
+string finalOutput;
+var digit = Convert.ToString(day);
+int lastDit = int.Parse(digit.Substring(digit.Length - 1));
+switch(lastDit)
+{
+    case 1:
+    theDay = ($"{day}st");
+    break;
+    
+    case 2:
+    theDay = ($"{day}nd");
+    break;
+
+    default:
+    theDay = ($"{day}th");
+    break;
+}
+finalOutput = ($"{theDay} day of {Seasons[season]} in the {year}");
+return finalOutput;
+}
+
+Console.WriteLine(CreateDayDescription(21, 3,1996));
+Console.WriteLine(CreateDayDescription(2, 1,1996));
+Console.WriteLine(CreateDayDescription(3, 2,1997));
+Console.WriteLine(CreateDayDescription(4, 0,1997));
