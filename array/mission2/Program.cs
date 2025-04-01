cdab10c1f18220e630837dfd281f87ec294fc4cf
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-04-01 17:31:00 +0200

    arrays mission 2

---
+++ array/mission2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numberOfMonster = new int[100];
+var random = new Random();
+
+for(int a = 0; a < 100; a++)
+{
+    numberOfMonster[a] = random.Next(1,51);
+}
+Array.Sort(numberOfMonster);
+Console.Write("Number of monsters in levels:");
+for(int a = 0; a < 99; a++)
+{
+    Console.Write($" {numberOfMonster[a]},");
+}
+Console.Write($" {numberOfMonster[99]}\n");
