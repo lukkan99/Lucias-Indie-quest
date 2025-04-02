17750d6b43e58ec1ebf5a7c7e738074761e1ee03
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-04-02 19:52:03 +0200

    wip

---
+++ array/mission3/Program.cs
@@ -0,0 +1,19 @@
+﻿int width = 5;
+int height = 5;
+
+var roads = new bool[width, height];
+
+static void GenerateRoad(bool[,] roads, int startX, int startY, int direction)
+{
+switch(direction)
+{
+    case 0:
+    break;
+    case 1:
+    break;
+    case 2:
+    break;
+    case 3:
+    break;
+}
+}
