c1476ccd766d335dbdc08c5b9735fb82c8199ef7
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-04-16 14:26:13 +0200

    dictionaries part 2

---
+++ Dictionary/mission2/Program.cs
@@ -0,0 +1,34 @@
+﻿var random = new Random();
+var capitals = new SortedList<string, string>();
+string contry;
+string city;
+int randomnumber;
+capitals["Australia"] = "Canberra";
+capitals["USA"] = "Washington DC";
+capitals["Greece"] = "Athens";
+capitals["Italy"] = "Rome";
+capitals["China"] = "Beijing";
+capitals["Canada"] = "Ottawa";
+capitals["United Kingdom"] = "London";
+capitals["Russia"] = "Moscow";
+capitals["Brazil"] = "Brasília";
+capitals["South Korea"] = "Seoul";
+capitals["Japan"] = "Tokyo";
+capitals["France"] = "Paris";
+
+for(int a = 0; a < 3; a++)
+{
+    randomnumber = random.Next(capitals.Count);
+    contry = capitals.GetKeyAtIndex(randomnumber);
+    city = capitals.GetValueAtIndex(randomnumber);
+    Console.WriteLine($"What is the capital of {contry}?");
+    string userinput = Console.ReadLine().ToLower();
+    if(userinput == city.ToLower())
+    {
+        Console.WriteLine("Correct!");
+    }
+    else
+    {
+        Console.WriteLine($"Incorrect! right awnser was {city}.");
+    }
+}
