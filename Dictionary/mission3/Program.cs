2837fac954d19ec8af0b20a4947b3431c078017c
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-04-17 16:26:14 +0200

    dicratroy mission 3

---
+++ Dictionary/mission3/Program.cs
@@ -0,0 +1,40 @@
+﻿var scores = new Dictionary<string, int>();
+bool hasinput = true; 
+
+while(hasinput)
+{
+    Console.Write("Who won this round?");
+    string userInput = Console.ReadLine();
+    Console.WriteLine();
+    if(userInput == "")
+    {
+        hasinput = false;
+    }
+    else
+    {
+        if(scores.ContainsKey(userInput))
+        {
+            scores[userInput] = scores[userInput] + 1;
+
+        }
+        else
+        {
+            scores.Add(userInput,1);
+        }
+    }
+var sortedPlayers = scores.Keys.OrderBy((player) => scores[player]).Reverse();
+
+if(hasinput)
+{
+    Console.WriteLine("RANKINGS");
+}
+else
+{
+    Console.WriteLine("FINAL RANKINGS");
+}
+foreach(string player in sortedPlayers)
+{
+    Console.WriteLine($"{player} {scores[player]}");
+}
+
+}
