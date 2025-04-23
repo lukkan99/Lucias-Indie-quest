4b6c86195af70c542f6e73ba6bc263958ca81e8f
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-04-23 15:48:36 +0200

    algorithmDesigns unit 1 mission 3

--- algorithmDesigns/unit1/mission3/Program.cs
+++ algorithmDesigns/unit1/mission3/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string ordinalNumbers(int nummber)
+{
+    int lastDiget = nummber % 10;
+    if(nummber > 10)
+    {
+        int secondLastDiget = nummber / 10 % 10;
+        if(secondLastDiget == 1)
+        {
+            return ($"{nummber}th");
+        }
+        else
+        {
+            switch(lastDiget)
+            {
+                default:
+                return($"{nummber}th");
+                case 1:
+                return($"{nummber}st");
+                case 2:
+                return($"{nummber}nd");
+                case 3:
+                return($"{nummber}rd");
+            }
+            
+
+        }
+    }
+    switch(lastDiget)
+            {
+                default:
+                return($"{nummber}th");
+                case 1:
+                return($"{nummber}st");
+                case 2:
+                return($"{nummber}nd");
+                case 3:
+                return($"{nummber}rd");
+            }
+
+}
+
+Console.WriteLine(ordinalNumbers(1));
+Console.WriteLine(ordinalNumbers(2));
+Console.WriteLine(ordinalNumbers(3));
+Console.WriteLine(ordinalNumbers(4));
+Console.WriteLine(ordinalNumbers(10));
+Console.WriteLine(ordinalNumbers(11));
+Console.WriteLine(ordinalNumbers(12));
+Console.WriteLine(ordinalNumbers(13));
+Console.WriteLine(ordinalNumbers(21));
+Console.WriteLine(ordinalNumbers(101));
+Console.WriteLine(ordinalNumbers(111));
+Console.WriteLine(ordinalNumbers(121));
