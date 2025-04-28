f9e7c87609378dcfe249814914618ba74095e306
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-04-28 15:11:17 +0200

    algorithmDesigns/unit2/mission2

---
+++ algorithmDesigns/unit2/mission2/Program.cs
@@ -0,0 +1,15 @@
+﻿static int Factorial(int n)
+{
+    if(n == 0)
+    {
+        return 1 ;
+    
+    }else
+    {
+        return (n * Factorial(n-1));
+    }
+}
+for(int a = 0; a <=10; a++)
+{
+Console.WriteLine($"{a}!{Factorial(a)}");
+}
