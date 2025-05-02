4857ecc1391eccf3113258ae2de80fcec59ea794
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-05-02 14:32:04 +0200

    starting with the code

--- algorithmDesigns/unit3/mission1/code/Program.cs
+++ algorithmDesigns/unit3/mission1/code/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var random = new Random();
+int width = 10;
+int height = 10;
+var roads = new bool[width, height];
+var river = new bool[width, height];
+var bidge = new bool[width, height];
+
+
+
+
+
+for (int y = 0; y < height; y++)
+{
+    for (int x = 0; x < width; x++)
+    {
+        // Decide which character to write and write it.
+    }
+
+    Console.WriteLine();
+}
