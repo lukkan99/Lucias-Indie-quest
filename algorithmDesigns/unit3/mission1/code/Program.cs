c64000922c1cc4bfc7f54bad203894d7200a4c0c
Lucia/Lukas Ekberg <7603446+lukkan99@users.noreply.github.com>
2025-05-02 15:56:09 +0200

    crappy update.
    brain not good today

--- algorithmDesigns/unit3/mission1/code/Program.cs
+++ algorithmDesigns/unit3/mission1/code/Program.cs
@@ -1,6 +1,9 @@
 ﻿var random = new Random();
-int width = 10;
+int width = 15;
 int height = 10;
+
+//2d arrys for all calc
+var forrest = new bool[width, height];
 var roads = new bool[width, height];
 var river = new bool[width, height];
 var bidge = new bool[width, height];
@@ -13,8 +16,12 @@ for (int y = 0; y < height; y++)
 {
     for (int x = 0; x < width; x++)
     {
-        // Decide which character to write and write it.
-    }
+        if((x==0 && y ==0) || (x == width-1 && y== height-1) || (x==0 && y==height-1) || (y==0 && x==width-1))
+        {
+            Console.Write("+");
+        }
+        Console.Write(" ");
 
+    }
     Console.WriteLine();
 }
